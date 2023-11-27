0c07aac510ac6731ae1915326099f2ddc7838458
Brent Ross Habawel <rosshabawel12@gmail.com>
2023-11-27 17:28:11 +0800

    added q&a&btn. added array for the content

---
+++ script.js
@@ -0,0 +1,50 @@
+const content = [
+  {
+    question: "What is the purpose of the 'typeof' operator in JavaScript?",
+    answer: "To determine the type of a variable",
+    choices: [
+      "To compare two values",
+      "To declare a variable",
+      "To loop through an array",
+    ],
+  },
+  {
+    question: "What is a closure in JavaScript?",
+    answer:
+      "A function that has access to its own scope, the outer function's scope, and the global scope",
+    choices: [
+      "A loop that runs infinitely",
+      "A variable declared with 'const'",
+      "An HTML tag",
+    ],
+  },
+  {
+    question: "What is the difference between 'let', 'const', and 'var'?",
+    answer:
+      "They are used to declare variables with different scoping rules and immutability",
+    choices: [
+      "They are interchangeable",
+      "They are used for mathematical operations",
+      "They represent different data types",
+    ],
+  },
+  {
+    question: "What is an arrow function in JavaScript?",
+    answer: "A concise way to write functions in ES6 syntax",
+    choices: [
+      "A function that always throws an error",
+      "A function used for sorting arrays",
+      "A type of loop",
+    ],
+  },
+  {
+    question: "What does the 'this' keyword refer to in JavaScript?",
+    answer: "The object that is currently executing the function",
+    choices: [
+      "The previous function in the call stack",
+      "The global object",
+      "The parent object",
+    ],
+  },
+  // Add more questions as needed
+];
